e04fc227b0f08c67a75a36f500b605502b2eb614
Carl Kim <carljkim@gmail.com>
2015-08-25 22:18:14 -0700

    Pass redux store to app

--- server.jsx
+++ server.jsx
@@ -1,30 +1,44 @@
-import express             from 'express';
-import React               from 'react';
-import { Router }          from 'react-router';
-import Location            from 'react-router/lib/location';
-import routes              from 'routes';
-import { applyMiddleware } from 'redux';
-import promiseMiddleware   from 'lib/promiseMiddleware'
+import express                          from 'express';
+import React                            from 'react';
+import { Router }                       from 'react-router';
+import Location                         from 'react-router/lib/location';
+import routes                           from 'routes';
+import { applyMiddleware }              from 'redux';
+import promiseMiddleware                from 'lib/promiseMiddleware'
+import { createStore, combineReducers } from 'redux';
+import { Provider }                     from 'react-redux';
+import * as reducers                    from 'reducers';
 
 const app = express();
 
 app.use((req, res) => {
   const location = new Location(req.path, req.query);
+  const reducer = combineReducers(reducers);
+  const store = createStore(reducer);
 
   Router.run(routes, location, (err, routeState) => {
     if (err) return console.error(err);
     if (!routeState) return res.status(404).end('404');
     
     const InitialComponent = (
-      <Router {...routeState} />
+      <Provider store={store}>
+        {() =>
+          <Router {...routeState} />
+        }
+      </Provider>
     );
     const componentHTML = React.renderToString(InitialComponent);
+    const initialState = store.getState();
     const HTML = `
     <!DOCTYPE html>
     <html>
       <head>
         <meta charset="utf-8">
         <title>Chore Split App</title>
+
+        <script type="application/javascript">
+          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
+        </script>
       </head>
       <body>
         <div id="react-view">${componentHTML}</div>
